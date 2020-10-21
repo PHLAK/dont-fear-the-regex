0bd231375c1a5f23b60d7f28a67af4250b918fcd
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2020-10-21 10:03:09 -0700

    Adding speaker notes and some other updates

--- src/js/deck.js
+++ src/js/deck.js
@@ -1,17 +1,18 @@
 import Reveal from 'reveal.js';
-import Markdown from 'reveal.js/plugin/markdown/markdown.esm.js';
 import RevealHighlight from 'reveal.js/plugin/highlight/highlight.esm';
+import RevealMarkdown from 'reveal.js/plugin/markdown/markdown.esm.js';
+import RevealNotes from "reveal.js/plugin/notes/notes.esm";
 
 let deck = new Reveal({
     hash: true,
-    plugins: [Markdown],
 });
 
 deck.initialize({
     width: 1080,
     height: 720,
     plugins: [
-        Markdown,
-        RevealHighlight
+        RevealHighlight,
+        RevealMarkdown,
+        RevealNotes
     ]
 });
